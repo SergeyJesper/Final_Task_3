fbadc2800bc27c8e2c7a04670cf3d888a7cf0070
SergeyJesper <jesperb@mail.ru>
2019-09-15 10:12:16 +0300

    Итог. задача 3

--- src/FinalTask3.java
+++ src/FinalTask3.java
@@ -1,5 +1,28 @@
+import java.math.*;
+import java.util.Scanner;
 public class FinalTask3 {
     public static void main(String[] arg) {
+        double rub = 0.0, usd = 0.0;
+        try {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите текущий курс доллара: ");
+        System.out.print("1 USD = ");
+        usd = in.nextDouble();
+          } catch (java.util.InputMismatchException e) {
+            System.out.println("Ошибка ввода: введите число!");
+        }
+        try {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите сумму в рублях для конвертации: ");
+            System.out.print("Сумма = ");
+            rub = in.nextDouble();
 
+        } catch (java.util.InputMismatchException e) {
+
+            System.out.println("Ошибка ввода: введите число!");
+
+        }
+        BigDecimal usdsum = new BigDecimal(rub / usd).setScale(2, RoundingMode.HALF_EVEN);
+        System.out.println("Рубли в доллары = " + usdsum);
     }
 }
